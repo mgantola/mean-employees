b043ce63a2d49e0a96cb2e8fa9370b201f9f7bf6
mgantola <mg.antola@gmail.com>
2018-09-18 01:59:22 -0300

    Added Nodemon and Morgan

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000,,() =>{
-    console.log('Server on port 3000');
+//settings
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+
+//Routes
+
+
+// Starting the server
+app.listen(app.get('port'),() =>{
+    console.log('Server on port',  app.get('port'));
 })
