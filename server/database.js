27e294ce1061b981dc112cf7fcdfcc73b2cee677
mgantola <mg.antola@gmail.com>
2018-09-18 02:14:01 -0300

    Adden mongoose and requires

---
+++ server/database.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+const URI = 'mongodb://localhost/mean-crud';
+
+mongoose.connect(URI);
--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 //settings
 app.set('port', process.env.PORT || 3000);
 
 //Middlewares
+app.use(morgan('dev'));
+app.use(express.json()); // Anteriormente se utilizaba body parser
 
 //Routes
 
